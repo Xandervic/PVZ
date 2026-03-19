f1c795710bb1eb79aad85954699d38685600493c
Xandervic <marj_cabalfajardo@yahoo.com>
2026-03-19 14:27:41 -0400

    Create PvZGame class

---
+++ core/src/com/xandervic/pvz/PvZGame.java
@@ -0,0 +1,9 @@
+// Main entry point of the game
+import com.badlogic.gdx.Game;
+
+public class PvZGame extends Game {
+    @Override
+    public void create() {
+        // Initialize your game here
+    }
+}
